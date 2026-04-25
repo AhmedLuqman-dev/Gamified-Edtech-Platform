683aef48382453bca11e8f4df76b3bccbdcf2468
AhmedLuqman-dev <you@example.com>
2026-04-25 11:36:17 +0530

    u9

--- backend/index.js
+++ backend/index.js
@@ -1242,6 +1242,6 @@ const runMigrations = async () => {
 
 runMigrations().then(() => {
   app.listen(process.env.PORT, () => {
-    console.log("Server running on port " + process.env.PORT);
+    // console.log("Server running on port " + process.env.PORT);
   });
 });
